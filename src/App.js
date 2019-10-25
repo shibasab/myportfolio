8e11c1920c0f82afc9f8d3587e620f45652d4541
shibasab <34082145+shibasab@users.noreply.github.com>
2019-10-25 15:08:32 +0900

    ひな型

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Navigation from './components/Navigation/Navigation';
+import About from './components/About/About';
+import Skills from './components/Skills/Skills';
+import Experience from './components/Experience/Experience';
+import Products from './components/Products/Products';
+import Contact from './components/Contact/Contact';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navigation />
+        <About />
+        <Skills />
+        <Experience />
+        <Products />
+        <Contact />
+      </div>
+    );
+  }
 }
-
 export default App;
