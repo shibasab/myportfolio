9be147d074a521a6be6d9ae650153ba9f31dccd3
shibasab <34082145+shibasab@users.noreply.github.com>
2019-10-30 21:51:52 +0900

    navigation

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,10 +1,33 @@
 import React from 'react';
-
+import './Navigation.css';
 //ナビゲーションバー
 //それぞれの要素のリンクを貼る
 //スクロールしても常に上に表示されるようにする
 const Navigation = () => {
-  return <h1>TODO : navバー</h1>;
+  return (
+    <ul className="topnav">
+      <li>
+        <a className="active" href="#home">
+          Top
+        </a>
+      </li>
+      <li>
+        <a href="#news">About</a>
+      </li>
+      <li>
+        <a href="#skills">Skills</a>
+      </li>
+      <li>
+        <a href="#experience">Experience</a>
+      </li>
+      <li>
+        <a href="#products">Products</a>
+      </li>
+      <li>
+        <a href="#contact">Contact</a>
+      </li>
+    </ul>
+  );
 };
 
 export default Navigation;
